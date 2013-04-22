3b4e6ead0a2c9226ab485aaf9d4b6aba6063869a
Ben Koski <bkoski@nytimes.com>
2013-04-22 17:54:11 -0400

    get rid of unneccessary describe blocks

--- spec/bitmap_pack_spec.rb
+++ spec/bitmap_pack_spec.rb
@@ -1,44 +1,41 @@
 require 'spec_helper'
+
 describe Tamper::BitmapPack do
 
-  describe "for multiple-choice data" do
+  before do
+    @data = [
+      { 'id' => 0, 'color' => ['yellow','blue'] },
+      { 'id' => 1, 'color' => ['blue', 'yellow'] },
+      { 'id' => 2, 'color' => ['red'] },
+    ]
 
-    before do
-      @data = [
-        { 'id' => 0, 'color' => ['yellow','blue'] },
-        { 'id' => 1, 'color' => ['blue', 'yellow'] },
-        { 'id' => 2, 'color' => ['red'] },
-      ]
+    @pack_set = Tamper::PackSet.new
+    @pack_set.add_attribute(:color, ['yellow', 'red', 'blue', 'purple'], 2)
+    @pack_set.pack!(@data)
 
-      @pack_set = Tamper::PackSet.new
-      @pack_set.add_attribute(:color, ['yellow', 'red', 'blue', 'purple'], 2)
-      @pack_set.pack!(@data)
+    @color_pack = @pack_set.pack_for(:color)
+    @color_pack.should be_a(Tamper::BitmapPack)
+  end
 
-      @color_pack = @pack_set.pack_for(:color)
-      @color_pack.should be_a(Tamper::BitmapPack)
-    end
+  its "item_window_width is equal to the number of possibilities, since multiple bits can be flipped for each item" do
+    @color_pack.item_window_width.should == ['yellow','red','blue','purple'].length
+  end
 
-    its "item_window_width is equal to the number of possibilities, since multiple bits can be flipped for each item" do
-      @color_pack.item_window_width.should == ['yellow','red','blue','purple'].length
-    end
+  its "length is equal to the number of choices * the number of items" do
+    @color_pack.bitset.size.should == (4 * 3)
+  end
 
-    its "length is equal to the number of choices * the number of items" do
-      @color_pack.bitset.size.should == (4 * 3)
-    end
+  it "flips a bit true for each selected choice" do
+    str_bits = @color_pack.bitset.to_s
 
-    it "flips a bit true for each selected choice" do
-      str_bits = @color_pack.bitset.to_s
+    # first item should have idx 0 set to true since it's yellow, idx 2 since it's blue
+    str_bits[0,4].should == '1010'
 
-      # first item should have idx 0 set to true since it's yellow, idx 2 since it's blue
-      str_bits[0,4].should == '1010'
+    # second item should have the same, order that possibilities are in does not matter
+    str_bits[4,4].should == '1010'
 
-      # second item should have the same, order that possibilities are in does not matter
-      str_bits[4,4].should == '1010'
-
-      # last item should have pos 2 set to true since it's red
-      str_bits[-4,4].should == '0100'
-    end
-
+    # last item should have pos 2 set to true since it's red
+    str_bits[-4,4].should == '0100'
   end
 
 end
--- spec/integer_pack_spec.rb
+++ spec/integer_pack_spec.rb
@@ -1,49 +1,47 @@
 require 'spec_helper'
+
 describe Tamper::IntegerPack do
 
-  describe "for single-choice data" do
+  before do
+    @data = [
+      { 'id' => 0, 'category_id' => '14' },
+      { 'id' => 1, 'category_id' => '21' },
+      { 'id' => 2, 'category_id' => '46' },
+    ]
 
-    before do
-      @data = [
-        { 'id' => 0, 'category_id' => '14' },
-        { 'id' => 1, 'category_id' => '21' },
-        { 'id' => 2, 'category_id' => '46' },
-      ]
+    @possibilities = (0...50).to_a.map(&:to_s)
 
-      @possibilities = (0...50).to_a.map(&:to_s)
+    @pack_set = Tamper::PackSet.new
+    @pack_set.add_attribute(:category_id, @possibilities, 1)
+    @pack_set.pack!(@data)
 
-      @pack_set = Tamper::PackSet.new
-      @pack_set.add_attribute(:category_id, @possibilities, 1)
-      @pack_set.pack!(@data)
-
-      @category_pack = @pack_set.pack_for(:category_id)
-      @category_pack.should be_a(Tamper::IntegerPack)
-    end
-
-    its "bit_window_width is equal to the number of bits required to represent the max possibility id" do
-      @category_pack.bit_window_width.should == @possibilities.length.to_s(2).length
-    end
-
-    its "item_window_width is to the bit_window_width" do
-      @category_pack.item_window_width.should == @category_pack.bit_window_width
-    end
-
-    its "overall length is equal to the item_window_width * number of items" do
-      @category_pack.bitset.size.should == @category_pack.item_window_width * 3
-    end
-
-    it "encodes the choice id in binary for each item" do
-      str_bits = @category_pack.bitset.to_s
-
-      # first item should be packed to represent 14
-      str_bits[0,6].should == 14.to_s(2).rjust(6, '0')
-
-      # next 21
-      str_bits[6,6].should == 21.to_s(2).rjust(6, '0')
-
-      # last item should be packed to represent 46
-      str_bits[-6,6].should == 46.to_s(2).rjust(6, '0')
-    end
-
+    @category_pack = @pack_set.pack_for(:category_id)
+    @category_pack.should be_a(Tamper::IntegerPack)
   end
+
+  its "bit_window_width is equal to the number of bits required to represent the max possibility id" do
+    @category_pack.bit_window_width.should == @possibilities.length.to_s(2).length
+  end
+
+  its "item_window_width is to the bit_window_width" do
+    @category_pack.item_window_width.should == @category_pack.bit_window_width
+  end
+
+  its "overall length is equal to the item_window_width * number of items" do
+    @category_pack.bitset.size.should == @category_pack.item_window_width * 3
+  end
+
+  it "encodes the choice id in binary for each item" do
+    str_bits = @category_pack.bitset.to_s
+
+    # first item should be packed to represent 14
+    str_bits[0,6].should == 14.to_s(2).rjust(6, '0')
+
+    # next 21
+    str_bits[6,6].should == 21.to_s(2).rjust(6, '0')
+
+    # last item should be packed to represent 46
+    str_bits[-6,6].should == 46.to_s(2).rjust(6, '0')
+  end
+
 end
