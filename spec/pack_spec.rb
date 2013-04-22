902f218593173107642314f0ca133879326a1648
Ben Koski <bkoski@nytimes.com>
2013-04-22 17:53:11 -0400

    copyedit

--- spec/pack_spec.rb
+++ spec/pack_spec.rb
@@ -3,14 +3,14 @@ describe Tamper::Pack do
 
   describe ".build" do
 
-    # Some sample scenarios...
+    # Some sample situations...
     describe "with a large number of possibilities" do
-      it "creates a BitmapPack when there are a large number of cohices allowed" do
-        Tamper::Pack.build(:category_id, (0...50).to_a, 50).should be_a(Tamper::BitmapPack)
+      it "creates an IntegerPack when only a single choice is allowed" do
+        Tamper::Pack.build(:category_id, (0...50).to_a, 1).should be_a(Tamper::IntegerPack)
       end
 
-      it "creates an IntegerPack when only a single choices is allowed" do
-        Tamper::Pack.build(:category_id, (0...50).to_a, 1).should be_a(Tamper::IntegerPack)
+      it "creates a BitmapPack when there are a large number of choices allowed" do
+        Tamper::Pack.build(:category_id, (0...50).to_a, 50).should be_a(Tamper::BitmapPack)
       end
     end
   end
