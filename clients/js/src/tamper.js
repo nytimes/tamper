744bf6fec95c37c471f9b474253d59af6d67301a
Ben Koski <ben.koski@gmail.com>
2014-04-11 14:36:16 -0700

    comment test harness requires

--- clients/js/src/tamper.js
+++ clients/js/src/tamper.js
@@ -1,7 +1,10 @@
+/* ---- begin test harness ---- */
 if (typeof module !== 'undefined' && module.exports) {
    var atob = require("../test/node_modules/atob"),
         _ =  require("../test/node_modules/underscore");
 }
+/* ---- end test harness ---- */
+
 
 Tamper = {}
 Tamper.biterate = function(encoded){
